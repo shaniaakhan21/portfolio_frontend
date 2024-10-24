699bfa54bf661c37942356d2cdbf8ff31c344e50
Shaniya Khan <shaniaakhan21@gmail.com>
2024-10-24 14:59:35 +0400

    Fix company post

--- src/app/application-to/[company_name]/page.tsx
+++ src/app/application-to/[company_name]/page.tsx
@@ -7,21 +7,23 @@ import BasicInfo from '@/app/components/Basicinfo';
 const CompanyPostsPage: React.FC<{ params: { company_name: string } }> = async ({ params }) => {
     const { company_name } = params;
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-
+    const normalizedCompanyName = company_name.toLowerCase();
     try {
         // Fetch posts for the specific company
-        const response = await axios.get(`${backendUrl}/application-to-${company_name}/`);
+        const response = await axios.get(`${backendUrl}/application-to-${normalizedCompanyName}/`);
         const posts = response.data;
+
         console.log("Fetched Posts for company:", posts);
+        console.log("company:", normalizedCompanyName);
 
         return (
             <div>
                 <BasicInfo className="hidden sm:block" />
-                <PostFeed company_name={company_name} posts={posts} />
+                <PostFeed company_name={normalizedCompanyName} posts={posts} />
             </div>
         );
     } catch (error) {
-        console.error("Error fetching posts:", error); // Log any fetch errors
+        console.error("Error fetching posts:", error); 
         return <div>Error loading posts</div>;
     }
 };
